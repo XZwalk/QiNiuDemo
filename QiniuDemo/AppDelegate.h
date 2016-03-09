acbd7c1d0683fd801fed443fbbd18d5326c83dfa
Nemo <nemo@nemodemac-mini.local>
2016-03-09 09:59:17 +0800

    Initial Commit

---
+++ QiniuDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QiniuDemo
+//
+//  Created by Nemo on 16/3/9.
+//  Copyright © 2016年 Nemo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
